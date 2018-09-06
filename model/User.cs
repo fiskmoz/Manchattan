0576ee9f9c8ab9b51aab26820ea8f455c4307db7
Anders Olsson <olsson.anders@protonmail.com>
2018-09-06 12:21:37 +0200

    Get and set added

--- model/User.cs
+++ model/User.cs
@@ -2,8 +2,25 @@
 
 public class User
 {
+    public struct user
+    {
+        int ID;
+        String name;
+        String password;
+        String mail;
+    }
+
+
 	public User()
 	{
+        ID++;
+    
 	}
+
+    // Get and set function for user.
+    public String name { get; set; }
+    public String password { get; set; }
+    public String mail { get; set; }
+
 
 }
