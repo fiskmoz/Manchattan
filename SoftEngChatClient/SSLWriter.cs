bb63b195fd990b08a997491d0cf008f3db62e481
Roman Eichler <roman.eichler1996@gmail.com>
2018-09-06 17:56:07 +0200

    SSLWriter: message construction.

--- SoftEngChatClient/SSLWriter.cs
+++ SoftEngChatClient/SSLWriter.cs
@@ -9,11 +9,39 @@ namespace SoftEngChatClient.Model.SSLCommunication
 {
 	class SSLWriter
 	{
-		private SslStream sslStream;
-
+		protected enum MessageType { loginUserName=0, loginUserPassword=1, client=2}
 		public SSLWriter(SslStream stream)
 		{
-			sslStream = stream;
+
 		}
+
+		public void Write(string message, MessageType type)
+		{
+			string outgoing = BuildMessage(type, message);
+			SendMessage(outgoing);
+		}
+
+		public void Write(string message, string client)
+		{
+			string outgoing = BuildMessage(MessageType.client, client, message);
+			SendMessage(outgoing);
+		}
+
+		private string BuildMessage(MessageType type, string client, string message)
+		{
+			string outgoing = type + "@" + client + ":" + message;
+			return outgoing;
+		}
+
+		private string BuildMessage(MessageType type, string message)
+		{
+			string outgoing = type + ":" + message;
+		}
+
+		private void SendMessage(string outgoing)
+		{
+
+		}
+
 	}
 }
