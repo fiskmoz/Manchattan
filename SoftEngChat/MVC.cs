c7683d67acc0daedc25fbe8b190f3556ae5b7ffb
Roman Eichler <roman.eichler1996@gmail.com>
2018-09-06 10:41:50 -0700

    Encryption: Server structure

---
+++ SoftEngChat/MVC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftEngChat
+{
+	namespace Model
+	{
+		namespace SSLCommunication { }
+	}
+	namespace View { }
+	namespace Controller{ }
+}
