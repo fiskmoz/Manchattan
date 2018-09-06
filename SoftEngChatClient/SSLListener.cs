11c33491308b0afa679bf3f32e1bb1ed8e9b10a7
Roman Eichler <roman.eichler1996@gmail.com>
2018-09-06 12:21:57 -0700

    SSLConnector: Client connect.

--- SoftEngChatClient/SSLListener.cs
+++ SoftEngChatClient/SSLListener.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,11 @@ namespace SoftEngChatClient.Model.SSLCommunication
 {
 	class SSLListener
 	{
+		private SslStream sslStream;
+
+		public SSLListener(SslStream sslStream)
+		{
+			this.sslStream = sslStream;
+		}
 	}
 }
