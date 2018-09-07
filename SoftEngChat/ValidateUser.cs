7a7848d2cf3c6359075bfc7595b99744c56e4a04
Nikla <Nikla@DESKTOP-MG8GK45>
2018-09-07 14:34:43 +0200

    Working validate

---
+++ SoftEngChat/ValidateUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftEngChat
+{
+    class ValidateUser
+    {
+        List<User> userlist;
+        public ValidateUser()
+        {
+            DatabaseManegement DB = new DatabaseManegement();
+            userlist = DB.DBread();
+
+        }
+        public Boolean validate(string userIn, string passwordIn)
+        {
+            foreach(var User in userlist)
+            {
+                if (User.name == userIn && User.password == passwordIn)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
