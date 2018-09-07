c64ee47e375eb1895eeadff28243d44451d4f6cf
Nikla <Nikla@DESKTOP-MG8GK45>
2018-09-07 15:21:31 +0200

    Commit

--- SoftEngChat/User.cs
+++ SoftEngChat/User.cs
@@ -2,21 +2,7 @@
 
 public class User
 {
-
-
-    public struct user
-    {
-        int ID;
-        String name;
-        String password;
-        String mail;
-    }
-
     public User() { ID++; }
-
-
-
-
 
 // Get and set function for user.
     public int ID { get;  set; }
