298a25e415e0a17c64fd5fbb4219033ebd038925
Roman Eichler <roman.eichler1996@gmail.com>
2018-09-09 11:13:18 +0200

    MessageHandler: Client-side !!!IMPORTANT!!! notes

--- SoftEngChatClient/Messagehandler.cs
+++ SoftEngChatClient/Messagehandler.cs
@@ -8,6 +8,14 @@ namespace SoftEngChatClient.Model.SSLCommunication
 		private string userName;
 		private SSLConnector server;
 
+		/********************************************************************************************/
+		/*		README:																				*/
+		/*			1.	Maybe catch incomming message event in driver instead of message handler?	*/
+		/*				Let driver use messagehandler-functions instead.							*/
+		/*			2.	Maybe raise events in MessageHandler for driver to catch depending			*/
+		/*				on type of incomming message?												*/
+		/********************************************************************************************/
+
 		//Handles messages arriving at Client.
 		public Messagehandler(string userName, SSLConnector server)
 		{
@@ -25,8 +33,9 @@ namespace SoftEngChatClient.Model.SSLCommunication
 			}
 
 			string incomming = Encoding.UTF8.GetString(message.Message); //Encoding!
-			
+
 			// Insert code how to handle a message here!
+			
 		}
 	}
 }
