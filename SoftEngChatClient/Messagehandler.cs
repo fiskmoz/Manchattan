819e3c1e20389a34ae64b23d513dad7a220db6ce
Roman Eichler <roman.eichler1996@gmail.com>
2018-09-13 10:23:43 +0200

    Client: MessageHandler: (probably) done.

--- SoftEngChatClient/Messagehandler.cs
+++ SoftEngChatClient/Messagehandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace SoftEngChatClient.Model.SSLCommunication
@@ -29,13 +30,49 @@ namespace SoftEngChatClient.Model.SSLCommunication
 			string incomming = Encoding.UTF8.GetString(message.Message); //Encoding!
 
 			if ( (int) incomming[0] == (int) MessageType.loginACK){
+				//Raises event to inform client if the login was accepted by the server.
+				RaiseEvent((int)incomming[1] == 0 ? false : true);
 			}
-			// Insert code how to handle a message here!
-			
+			else if((int)incomming[0] == (int)MessageType.incommingClient)
+			{
+				ClientMessage(incomming);
+			}
 		}
 
-		public delegate void EventHandler(Object sender, LoginValid eventArgs);
-		public event EventHandler LoginValid;
+		//Raises new event containing a message for the client
+		private void ClientMessage(string incomming)
+		{
+			List<string> list = ParseIncomming(incomming);
+			RaiseEvent(list[0], list[1]);
+		}
+
+		//Parses incomming client message
+		private List<string> ParseIncomming(string incomming)
+		{
+			List<string> list = new List<string>();
+			int i;
+			string sender = null;
+			string message = null;
+			StringBuilder stringBuilder = new StringBuilder();
+			for (i = 1; incomming[i] != ':'; i++)
+			{
+				sender += incomming[i];
+			}
+			for (int j = ++i; j < incomming.Length; j++)
+			{
+				message += incomming[j];
+			}
+			list.Add(sender);
+			list.Add(message);
+
+			return list;
+		}
+
+		public delegate void LoginEventHandler(Object sender, LoginValid eventArgs);
+		public delegate void IncommingEventHandler(Object sender, ParsedIncommingMessage eventArgs);
+
+		public event LoginEventHandler LoginValid;
+		public event IncommingEventHandler ParsedIncommmingMessage;
 
 		private void RaiseEvent(bool isValid)
 		{
@@ -44,10 +81,28 @@ namespace SoftEngChatClient.Model.SSLCommunication
 			LoginValid(this, flag);
 		}
 
+		private void RaiseEvent(string sender, string message)
+		{
+			ParsedIncommingMessage messageEvent = new ParsedIncommingMessage(sender, message);
+			ParsedIncommmingMessage(this, messageEvent);
+		}
+
 	}
 
 	class LoginValid : EventArgs
 	{
 		public bool isValid;
+	}
+
+	class ParsedIncommingMessage : EventArgs
+	{
+
+		public string sender;
+		public string message;
+		public ParsedIncommingMessage(string sender, string message)
+		{
+			this.sender = sender;
+			this.message = message;
+		}
 	}
 }
