f3cca55d742ee3e76ccdb85c195e7532036e2c9f
Nikla <Nikla@DESKTOP-MG8GK45>
2018-09-14 08:06:23 +0200

    commit

--- SoftEngChat/User.cs
+++ SoftEngChat/User.cs
@@ -2,11 +2,8 @@
 
 public class User
 {
-
-
-
     private static int IDnr;
-    public int ID { get;  set; }
+    public int ID { get;  private set; }
     public string name { get; set; }
     public string password { get; set; }
     public string mail { get; set; }
