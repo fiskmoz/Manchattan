fa62ae4dbf3419b641bd4077bf2416c838765533
Nikla <Nikla@DESKTOP-MG8GK45>
2018-09-20 14:15:30 +0200

    Session Created

---
+++ SoftEngChatClient/Session.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftEngChatClient
+{
+    class Session
+    {
+        public int sessionID { get; set; };
+        private string sessionUser;
+        private string userPassword;
+        private bool rememberMe;
+        public Session(string username, string password, bool saveInfo, int sessionCounter)
+        {
+            sessionID = ++sessionCounter;
+            sessionUser = username;
+            userPassword = password;
+            rememberMe = saveInfo;
+
+            if(openNewSession(sessionID, sessionUser))
+            {
+                if(checkRememberMe(rememberMe))
+                    printRememberMeToFile(sessionUser, userPassword);
+
+                
+                // Gör saker
+            }
+            else
+                //terminate instance of Class
+            
+            
+        }
+        public bool openNewSession(int SID, string user)
+        {
+            return getSessionAck(SID, user);
+        }
+        private bool checkRememberMe(bool remember)
+        {
+            if (remember == true)
+                return true;
+            else
+                return false;
+                
+        }
+        private void printRememberMeToFile(string user, string pass)
+        {
+            //print to file
+        }
+    }
+}
