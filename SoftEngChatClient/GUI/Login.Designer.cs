121be9c8ef956b6516e9d62d3bd84325dd9c7d15
Roman Eichler <roman.eichler1996@gmail.com>
2018-09-20 11:29:31 +0200

    LoginGUI: RegistartionInfo Lable is now Green.

--- SoftEngChatClient/GUI/Login.Designer.cs
+++ SoftEngChatClient/GUI/Login.Designer.cs
@@ -161,6 +161,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Green;
 			this.label1.Location = new System.Drawing.Point(333, 190);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(153, 17);
