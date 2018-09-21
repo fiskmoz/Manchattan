f2420b9841b14054493b888a0c037437c28315d9
Nikla <Nikla@DESKTOP-MG8GK45>
2018-09-21 13:45:02 +0200

    commit

--- SoftEngChatClient/ClientDriver.cs
+++ SoftEngChatClient/ClientDriver.cs
@@ -141,6 +141,7 @@ namespace SoftEngChatClient.Controller
 
             if (loginCredentials == "")
             {
+                loginWindow.rememberMeCheckBox.Checked = false;
                 return;
             }
             string[] userInfo;
