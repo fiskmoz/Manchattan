31cf9dade829049a7ea48f6dca28bb579507d0fd
Anders Olsson <olsson.anders@protonmail.com>
2018-10-04 15:19:16 +0200

    Database test

--- UnitTestJsonParser/UnitTest1.cs
+++ UnitTestJsonParser/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SoftEngChat;
+using System;
 using System.Collections.Generic;
 
 namespace UnitTestJsonParser
@@ -8,13 +9,28 @@ namespace UnitTestJsonParser
     public class UnitTest1
     {
         private DatabaseManegement dataBase = new DatabaseManegement();
+        private static readonly string fileName = "DB.txt";
+        private static string filePath = AppDomain.CurrentDomain.BaseDirectory + @"\" + fileName;
         private List<User> userList = new List<User>();
 
         [TestMethod]
         public void readFromFile()
         {
-            userList = dataBase.DBread("Siafjosjfjaojs");
-            Assert.IsNotNull(userList);
+            List<string> info = new List<string>
+            {
+                "Admin",
+                "mail",
+                "112",
+                "Admin",
+                "Adminson"
+            };
+            userList.Add(new User(info));
+
+            // Assert.AreEqual(userList, dataBase.DBread(filePath));
+            Assert.IsNotNull(dataBase.DBread(filePath));
+
+
+
         }
     }
 }
