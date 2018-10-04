0647c709a5a7045dfe80efdb4c3a957e6491b69a
Roman Eichler <roman.eichler1996@gmail.com>
2018-10-04 14:50:29 +0200

    RegisterDriver: using SSLCom.

--- SoftEngChatClient/Drivers/RegisterDriver.cs
+++ SoftEngChatClient/Drivers/RegisterDriver.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using SoftEngChatClient.Model;
+using SoftEngChatClient.Model.SSLCommunication;
 
 namespace SoftEngChatClient.Controller
 {
