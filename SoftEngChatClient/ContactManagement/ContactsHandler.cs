b37c8c6c57486e5b5d0d01e99ba86b2aff51d2aa
Roman Eichler <roman.eichler1996@gmail.com>
2018-10-05 14:45:25 +0200

    ContactHandler.

--- SoftEngChatClient/ContactManagement/ContactsHandler.cs
+++ SoftEngChatClient/ContactManagement/ContactsHandler.cs
@@ -10,31 +10,54 @@ namespace SoftEngChatClient.Model
 	{
 		public event EventHandler UpdateContactList;
 		private List<Contact> contactList;
+		private List<string> offlineList;
+		private List<string> onlineList;
+		private FileManager fileManager;
 
-		public ContactsHandler()
+		private bool hasOfflineList = false;
+		private bool hasOnlineList = false;
+
+		public ContactsHandler(FileManager fm)
 		{
 			contactList = new List<Contact>();
+			onlineList = new List<string>();
+			offlineList = new List<string>();
+			fileManager = fm;
 		}
 
 		public void Subscribe(Messagehandler mh)
 		{
 			mh.IncommingOnlineList += new EventHandler(HandleOnlineList);
+			mh.IncommingOfflineList += new EventHandler(HandleOfflineList);
 		}
 
 		private void HandleOnlineList(object sender, EventArgs e)
 		{
-			UpdateContactList(this, new ContactListEventArg(CreateContactList(((OnlineList)e).onlineList)));
+			hasOnlineList = true;
+			onlineList = ((OnlineList)e).onlineList;
+			contactList = CreateContactList();
+
+			UpdateContactList(this, new ContactListEventArg(contactList));
 
 		}
 
-		private List<Contact> CreateContactList(List<string> onlineList)
+		private void HandleOfflineList(object sender, EventArgs e)
 		{
-			foreach( string i in onlineList)
+			hasOfflineList = true;
+			offlineList = ((OnlineList)e).onlineList;
+		}
+
+		private List<Contact> CreateContactList()
+		{
+			List<string> contacts = fileManager.ReadContacts();
+			List<Contact> tempContactList = new List<Contact>();
+			foreach(string contact in contacts)
 			{
-				contactList.Add(new Contact(i, true));
+				tempContactList.Add(new Contact(contact, onlineList.Contains(contact)));
 			}
 
-			return contactList;
+			return tempContactList;
+
 		}
 	}
 }
